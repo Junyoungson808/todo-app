502c7994746712c79c1ec5b440d757d31e34c2ac
Junyoung Son <junyoungson808@gmail.com>
2022-11-01 03:27:19 +0000

    update

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { createStyles } from '@mantine/core';
 // import './header.scss';
 
+const HEADER_HEIGHT = 84;
 
 const Header = () => {
+
+  const useStyles = createStyles((theme) => ({
+    header: {
+      backgroundColor: theme.fn.variant({ variant: 'filled', color: theme.primaryColor }).background,
+      borderBottom: 0,
+    },
+
+    inner: {
+      height: HEADER_HEIGHT,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    }
+
+  })
+  )
+
   return(
     <>
-    <header>
+    <header id="header-main">
       <h1>Home</h1>
     </header>
     </>
